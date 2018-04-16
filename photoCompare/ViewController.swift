4ccd4182ed322f4b2523828f2ce0bb69ee5c4a09
ericyu423 <ericyu423@hotmail.com>
2018-04-16 09:19:11 -0700

    added 2nd way to add image to alertView

--- photoCompare/ViewController.swift
+++ photoCompare/ViewController.swift
@@ -52,7 +52,7 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
+  
         image = info[UIImagePickerControllerEditedImage] ?? info[UIImagePickerControllerOriginalImage]
         //imageView.image = image as? UIImage
 
@@ -69,14 +69,14 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
            //print("number \(vp.numberFaceDetected(image: imageToprocess))")
             switch (vp.numberFaceDetected(image: imageToprocess)){
             case 0:
-                msg = "no face detected, please retake photo"
+                msg = "no face detected, please retake photo\n"
             case 1:
                 msg = ""
             case let x where x > 1:
-                msg = "multiple faces detected, please retake photo"
+                msg = "multiple faces detected, please retake photo\n"
  
             default:
-                msg = "error, please retake photo"
+                msg = "error, please retake photo\n"
 
             }
             
@@ -84,24 +84,38 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
             dismiss(animated: true, completion: {
 
                 
-                let alertController = UIAlertController(title: "photo verification", message: msg, preferredStyle: .alert)
+                let numberOfBlankLines = 12
+                let maxImageSize = CGSize(width: numberOfBlankLines * 16 , height: numberOfBlankLines * 16) //about 16 pt per return character
+                let returnCharacters = String(repeating: "\n", count: numberOfBlankLines)
+                let alertController = UIAlertController(title: "photo verification", message: msg + returnCharacters, preferredStyle: .alert)
+        
+                let imageView = UIImageView(image: self.image as? UIImage)
                 
-                 alertController.addImage(image: imageToprocess)
+                let scaleSize = self.imageScaleSize(maxSize: maxImageSize ,image:(self.image as? UIImage))
                 
+                 alertController.view.addSubview(imageView)
+                
+    
                 if (vp.numberFaceDetected(image: imageToprocess) == 1){
                 alertController.addAction(UIAlertAction(title: "Accepted", style: .default, handler: { _ in
                     //this should probably call the regular check-in
                 }))
                 }else{
-                   
+                    alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
+                        //does nothing
+                    }))
                     alertController.addAction(UIAlertAction(title: "Retry", style: .default, handler: { _ in
                         self.verifyUserImage()
                     }))
-                    alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { _ in
-                        //does nothing
-                    }))
+                  
                 }
                 
+                //let heightConstant: CGFloat = 16 * 8 //8 return character
+                imageView.translatesAutoresizingMaskIntoConstraints = false
+                alertController.view.addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerX, relatedBy: .equal, toItem: alertController.view, attribute: .centerX, multiplier: 1, constant: 0))
+                alertController.view.addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal, toItem: alertController.view, attribute: .centerY, multiplier: 1, constant: 0))
+                alertController.view.addConstraint(NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: scaleSize.width ))
+                alertController.view.addConstraint(NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: scaleSize.height ))
                 
                 
                 self.present(alertController, animated: true, completion: nil)
@@ -112,6 +126,21 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
       
         self.dismiss(animated: true, completion:nil)
         
+    }
+    private func imageScaleSize(maxSize: CGSize,image:UIImage?)->CGSize {
+        
+        let imgSize = image?.size ?? maxSize
+        
+        var ratio:CGFloat!
+        if(imgSize.width > imgSize.height){
+            ratio = maxSize.width/imgSize.width
+        }else {
+            ratio = maxSize.height / imgSize.height
+        }
+        
+        let scaleSize = CGSize(width: imgSize.width * ratio, height: imgSize.height * ratio)
+        
+        return scaleSize
     }
     
     
@@ -126,26 +155,32 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
 }
 
 extension UIAlertController {
+    
+   
     func addImage(image: UIImage) {
         let maxSize = CGSize(width: 200, height: 200)
         let imgSize = image.size
-        
         var ratio:CGFloat!
         if(imgSize.width > imgSize.height){
             ratio = maxSize.width/imgSize.width
         }else {
            ratio = maxSize.height / imgSize.height
         }
-        
         let scaleSize = CGSize(width: imgSize.width * ratio, height: imgSize.height * ratio)
         let resizeImage = image.imageWithSize(scaleSize)
-        
+
         let imgAction = UIAlertAction(title: "", style: .default, handler: nil)
         imgAction.isEnabled = false
-        imgAction.setValue(resizeImage.withRenderingMode(.alwaysOriginal), forKey: "image")
+        
+        imgAction.setValue(image, forKey: "image")
+        
+        //image is private no good
+     
+    
         self.addAction(imgAction)
      
     }
+    
 }
 
 
